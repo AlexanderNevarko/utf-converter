d221986fac16f2a26cd7b0ba6a5e3d9d804135a6
Alexander <aleksandr.nevarko@frtk.ru>
2019-11-26 18:53:58 +0300

    modified:   Makefile 	modified:   converters.cpp 	modified:   test.cpp

--- src/converters.cpp
+++ src/converters.cpp
@@ -7,17 +7,17 @@ std::vector<uint8_t> to_utf8(const std::vector<uint32_t> &x) {
     int len = x.size();
     for (int i = 0; i < len; i++) {
         uint32_t elem = x[i];
-        if (elem > 0 && elem < 128) {
+        if (elem > 0 && elem < 0x80) {
             utf_vec.push_back(uint8_t(x[i]));
         }
-        else if (elem < 2048) {
+        else if (elem < 0x800) {
             uint8_t byte1, byte2;
             byte2 = uint8_t((elem & 0x0000003F) | 0x00000080);
             byte1 = uint8_t(((elem & 0x000007C0) >> 6) | 0x000000C0);
             utf_vec.push_back(byte1);
             utf_vec.push_back(byte2);
         }
-        else if (elem < 65536) {
+        else if (elem < 0x10000) {
             uint8_t byte1, byte2, byte3;
             byte3 = uint8_t((elem & 0x0000003F) | 0x00000080);
             byte2 = uint8_t(((elem & 0x00000FC0) >> 6) | 0x00000080);
@@ -26,7 +26,7 @@ std::vector<uint8_t> to_utf8(const std::vector<uint32_t> &x) {
             utf_vec.push_back(byte2);
             utf_vec.push_back(byte3);
         }
-        else if (elem < 1112065) {
+        else if (elem < 0x1FFFFF) {
             uint8_t byte1, byte2, byte3, byte4;
             byte4 = uint8_t((elem & 0x0000003F) | 0x00000080);
             byte3 = uint8_t(((elem & 0x00000FC0) >> 6) | 0x00000080);
@@ -38,7 +38,7 @@ std::vector<uint8_t> to_utf8(const std::vector<uint32_t> &x) {
             utf_vec.push_back(byte4);
 
         }
-        else if (elem < 0x7FFFFFFF) {
+        else if (elem < 0x3FFFFFF) {
             uint8_t byte1, byte2, byte3, byte4, byte5;
             byte5 = uint8_t((elem & 0x0000003F) | 0x00000080);
             byte4 = uint8_t(((elem & 0x00000FC0) >> 6) | 0x00000080);
@@ -73,5 +73,59 @@ std::vector<uint8_t> to_utf8(const std::vector<uint32_t> &x) {
 
 std::vector<uint32_t> from_utf8(const std::vector<uint8_t> &x) {
     std::vector<uint32_t> ret_vec;
+    int len = x.size();
+    int i = 0;
+    while (i < len) {
+        uint8_t elem = x[i];
+        if (elem >> 7 == 0) {
+            ret_vec.push_back(uint32_t(elem));
+            i++;
+        }
+        else if (elem >> 5 == 0b110) {
+            uint32_t qbyte1, qbyte2;
+            qbyte1 = uint32_t(x[i] & 0x1F) << 6;
+            qbyte2 = uint32_t(x[i+1] & 0x3F);
+            ret_vec.push_back(qbyte1 + qbyte2);
+            i += 2;
+        }
+        else if (elem >> 4 == 0b1110) {
+            uint32_t qbyte1, qbyte2, qbyte3;
+            qbyte1 = uint32_t(x[i] & 0x0F) << 12;
+            qbyte2 = uint32_t(x[i+1] & 0x3F) << 6;
+            qbyte3 = uint32_t(x[i+2] & 0x3F);
+            ret_vec.push_back(qbyte1 + qbyte2 + qbyte3);
+            i += 3;
+        }
+        else if (elem >> 3 == 0b11110) {
+            uint32_t qbyte1, qbyte2, qbyte3, qbyte4;
+            qbyte1 = uint32_t(x[i] & 0x07) << 18;
+            qbyte2 = uint32_t(x[i+1] & 0x3F) << 12;
+            qbyte3 = uint32_t(x[i+2] & 0x3F) << 6;
+            qbyte4 = uint32_t(x[i+3] & 0x3F);
+            ret_vec.push_back(qbyte1 + qbyte2 + qbyte3 + qbyte4);
+            i += 4;
+        }
+        else if (elem >> 2 == 0b111110) {
+            uint32_t qbyte1, qbyte2, qbyte3, qbyte4, qbyte5;
+            qbyte1 = uint32_t(x[i] & 0x03) << 24;
+            qbyte2 = uint32_t(x[i+1] & 0x3F) << 18;
+            qbyte3 = uint32_t(x[i+2] & 0x3F) << 12;
+            qbyte4 = uint32_t(x[i+3] & 0x3F) << 6;
+            qbyte5 = uint32_t(x[i+4] & 0x3F);
+            ret_vec.push_back(qbyte1 + qbyte2 + qbyte3 + qbyte4 + qbyte5);
+            i += 5;
+        }
+        else {
+            uint32_t qbyte1, qbyte2, qbyte3, qbyte4, qbyte5, qbyte6;
+            qbyte1 = uint32_t(x[i] & 0x01) << 30;
+            qbyte2 = uint32_t(x[i+1] & 0x3F) << 24;
+            qbyte3 = uint32_t(x[i+2] & 0x3F) << 18;
+            qbyte4 = uint32_t(x[i+3] & 0x3F) << 12;
+            qbyte5 = uint32_t(x[i+4] & 0x3F) << 6;
+            qbyte6 = uint32_t(x[i+5] & 0x3F);
+            ret_vec.push_back(qbyte1 + qbyte2 + qbyte3 + qbyte4 + qbyte5 + qbyte6);
+            i += 6;
+        }
+    }
     return ret_vec;
 }
--- src/test.cpp
+++ src/test.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
+#include <iomanip>
+#include <assert.h>
 #include "converters.hpp"
 
 int main() {
-    std::vector<std::uint32_t> vec(5, 2324);
-    std::vector<std::uint8_t> utf_vec = to_utf8(vec);
-    for (int i = 0; i < utf_vec.size(); i++) {
-        printf("%d ", utf_vec[i]);
+    std::vector<std::uint32_t> vec = {3212, 32, 32413, 543};
+    std::vector<std::uint32_t> _vec = from_utf8(to_utf8(vec));
+    assert(vec.size() == _vec.size());
+    for (int i = 0; i < vec.size(); i++) {
+        //printf("orig: %d, from_utf8(to_utf8(orig)): %d\n ", vec[i], _vec[i]);
+        std::cout << "original" << std::setw(12) << vec[i] << std::setw(30)
+        << "from_utf8(to_utf8(orig))" << std::setw(12) << _vec[i] << std::endl;
     }
     
     return 0;
