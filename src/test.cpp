80654fdd2d96706d995aba2e80e654eb94d785ed
Alexander <aleksandr.nevarko@frtk.ru>
2019-11-26 17:12:45 +0300

    new file:   Makefile         Makefile made

--- src/test.cpp
+++ src/test.cpp
@@ -2,7 +2,7 @@
 #include "converters.hpp"
 
 int main() {
-    std::vector<std::uint32_t> vec(5, 23424);
+    std::vector<std::uint32_t> vec(5, 2324);
     std::vector<std::uint8_t> utf_vec = to_utf8(vec);
     for (int i = 0; i < utf_vec.size(); i++) {
         printf("%d ", utf_vec[i]);
